cf9f99d720c9e928d666a4783cd0e592873f230e
Neil Sarkar <neil@groupme.com>
2013-06-15 00:41:11 -0400

    WIP - dealing with touch events

--- app.rb
+++ app.rb
@@ -7,9 +7,18 @@ class App < Sinatra::Base
 
   get "/" do
     <<-html
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
     <style type="text/css">body {padding: 0; margin: 0; cursor: pointer;} </style>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/zepto/1.0/zepto.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="/javascripts/hammer.js"></script>
     <script src="/javascripts/line.js"></script>
+    <script>
+      console.debug = function(text) {
+        $("#debug").append(text)
+        $("#debug").append("<br />")
+      }
+    </script>
+    <p id="debug" style="background-color:black; color:green; font-family: Helvetica"></p>
     html
   end
 end
--- app/javascripts/line.js
+++ app/javascripts/line.js
@@ -1,32 +1,55 @@
 $(function() {
   var bounds = $(window)
 
-  var canvas = $("<canvas></canvas>")
-  canvas.attr({width: bounds.width()})
-  canvas.attr({height: bounds.height()})
+  var $canvas = $("<canvas></canvas>")
+  var canvas = $canvas[0]
 
-  $("body").append(canvas)
+  $canvas.attr({width: bounds.width()})
+  $canvas.attr({height: bounds.height()})
 
-  var context = canvas[0].getContext("2d");
+  $("body").append($canvas)
+
+  var context = $canvas[0].getContext("2d");
   context.fillStyle="#7722ff";
-  context.fillRect(0,0,10000,1);
+  context.fillRect(0,0,10000,20);
 
   window.context = context;
   var points = {}
 
-  canvas.mousedown(function(e) {
-    e.preventDefault()
-    points.start = {
-      x: e.x,
-      y: e.y
-    }
-  })
+  var isPhone = $(window).width() <= 640;
 
-  canvas.mouseup(function(e) {
-    context.beginPath()
-    context.moveTo(points.start.x, points.start.y)
-    context.lineTo(e.x, e.y)
-    context.strokeStyle = "#000"
-    context.stroke()
-  })
+  if( !isPhone ) {
+    $canvas.mousedown(function(e) {
+      e.preventDefault()
+      points.start = {
+        x: e.x,
+        y: e.y
+      }
+    })
+
+    $canvas.mouseup(function(e) {
+      context.beginPath()
+      context.moveTo(points.start.x, points.start.y)
+      context.lineTo(e.x, e.y)
+      context.strokeStyle = "#000"
+      context.stroke()
+    })
+  } else {
+    canvas.addEventListener("touchstart", function(e) {
+      e.preventDefault()
+      points.start = {
+        x: e.pageX,
+        y: e.pageY
+      }
+    }, false)
+
+    canvas.addEventListener("touchend", function(e) {
+      context.beginPath()
+      context.moveTo(points.start.x, points.start.y)
+      context.lineTo(e.pageX, e.pageY)
+      context.strokeStyle = "#000"
+      context.stroke()
+      alert("went from " + points.start.x + "," + points.start.y + " to " + e.pageX + "," + e.pageY)
+    }, false)
+  }
 })
