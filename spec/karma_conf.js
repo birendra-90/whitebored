05536e88ea2d661dcd107fb9157c170a9f2ae23e
Neil Sarkar <neil@groupme.com>
2013-06-18 21:43:46 -0400

    Fix footer positioning

--- spec/karma_conf.js
+++ spec/karma_conf.js
@@ -3,7 +3,6 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'spec/lib/faye.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js',
   'http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js',
   'http://cdn.pubnub.com/pubnub-3.5.1.min.js',
